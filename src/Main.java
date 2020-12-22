9288e7d15a92fb313cde84c5d64eeb1ca456e535
balrom1981 <balrom@bk.ru>
2020-12-22 16:39:27 +0300

    README added

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int addition = 15000;
+        int cost = 100;
+        int bonus = addition / cost;
+        int limit = 1000;
+        int total;
+        if (addition > limit) {
+            total = amount + addition + bonus;
+        } else {
+              total = amount + addition;}
+    System.out.println(total);
+}
+}
